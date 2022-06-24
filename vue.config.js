a3fc5d13f5b0dea03006fa0a8284b8bd25e0be35
hugoriveros18 <hugoriverosfajardo@gmail.com>
2022-06-23 19:31:32 -0500

    config changes

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/ultimate-calculator/'
-    : '/'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
